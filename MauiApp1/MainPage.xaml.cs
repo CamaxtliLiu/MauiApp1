314f03de5b58955f740f8e6462594ccda8b2485c
Liu Kuang Ming <kuangming0917@gmail.com>
2023-12-01 16:00:53 +0800

    PTAdd .gitattributes and .gitignore.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnReadMoreTapped(object sender, EventArgs e)
+        {
+            // Check if the full text is currently visible
+            bool isFullTextVisible = fullTextLabel.IsVisible;
+
+            // Toggle the visibility of the full text
+            fullTextLabel.IsVisible = !isFullTextVisible;
+
+            // Update the "Read More/Read Less" label text
+            readMoreLabel.Text = isFullTextVisible ? "Read More" : "Read Less";
+
+            // Optionally, adjust the summary label visibility if needed
+            summaryLabel.IsVisible = isFullTextVisible;
+        }
+    }
+}
